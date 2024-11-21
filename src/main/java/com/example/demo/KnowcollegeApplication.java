6973f8372b7e784fb6d835bacd23861468f90c32
Caueobm <caue.obm@gmail.com>
2024-11-21 20:14:56 -0300

    Primeiro commit

---
+++ src/main/java/com/example/demo/KnowcollegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowcollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowcollegeApplication.class, args);
+	}
+
+}
