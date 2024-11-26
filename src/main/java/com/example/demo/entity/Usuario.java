e256e1bfc87816c47c30cc1322986c072a842ad7
caueobm <caue.obm@gmail.com>
2024-11-25 21:28:45 -0300

    Classes Repositories Criadas

--- src/main/java/com/example/demo/entity/Usuario.java
+++ src/main/java/com/example/demo/entity/Usuario.java
@@ -2,9 +2,17 @@ package com.example.demo.entity;
 
 import javax.persistence.*;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
@@ -22,6 +30,4 @@ public class Usuario {
 
     @OneToMany(mappedBy = "usuario")
     private Set<Compra> compras;
-
-    // Getters e Setters
 }
